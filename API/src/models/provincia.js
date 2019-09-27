ccf9435d267e411a93334f6b133a9fa09ce10c31
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-09-27 18:52:43 +0200

    Provincia deleted

--- API/src/models/provincia.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("../../util/bdConnection");
-
-var provincia = new mongoose.Schema({
-    nombre : String
-});
-
-var Provincia = mongoose.model("Provincia",provincia,"provincias");
-
-module.exports = Provincia;
