96b14a27ee842b4e244703c0930269830c442171
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-10-01 19:35:29 +0200

    Commit

--- APP/app_zainduz/src/App.js
+++ APP/app_zainduz/src/App.js
@@ -6,25 +6,11 @@ const cabecera = require("./util/headerAxios");
 class App extends React.Component {
   state = {
     respuesta: "No especificado",
-    hecho: false
+    hecho: "Hola mudno"
   };
 
   componentDidMount(){
-    axios({
-      method: "PATCH",
-      url: 'http://' + ipMaquina + ':3001/updateRow/cuidador/5d935550bb3b770558c2510b',
-      headers: cabecera,
-      data: {
-        nombre: "Antonius"
-      }
-    }).then(res => {
-        this.setState({
-          respuesta: res.data,
-          hecho: true
-        });
-    }).catch(err => {
-        this.setState({ respuesta: err.message, hecho: "ERROR" });
-    });
+    
   }
 
   render() {
