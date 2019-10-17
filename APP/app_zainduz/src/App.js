cce09f30e30b49d0c06e592cc78739db5b2b47e6
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-10-17 15:57:16 +0200

    Pruebas con la tabla

--- APP/app_zainduz/src/App.js
+++ APP/app_zainduz/src/App.js
@@ -3,32 +3,35 @@ import { slide as BurgerMenu } from "react-burger-menu";
 import Headroom from "react-headroom";
 import Avatar from "react-avatar";
 import SlideTab from "./components/slideTab";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tabla from "autoresponsive-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes, faPhone } from "@fortawesome/free-solid-svg-icons";
 import "./components/styles/header.css";
+import "./components/styles/menuPerfil.css";
 
 class App extends React.Component {
   constructor(props) {
-    super(props);    
+    super(props);
     this.state = {
       isOpenMenuPerfil: false
     };
   }
-  handleStateChange (state) {
-    this.setState({isOpenMenuPerfil: state.isOpen})  
+  handleStateChange(state) {
+    this.setState({ isOpenMenuPerfil: state.isOpen });
   }
 
   //Abre o cierra el menu segun el estado actual
   toggleMenu() {
-    this.setState(state => ({isOpenMenuPerfil: !state.isOpenMenuPerfil}))
+    this.setState(state => ({ isOpenMenuPerfil: !state.isOpenMenuPerfil }));
   }
 
   render() {
-    
     return (
       <div id="outer-container" style={{ height: 2000 }} className="w-100">
         <BurgerMenu
-          customCrossIcon = {<i className={"far fa-times-circle"}></i>}
-          onStateChange = {(state) => this.handleStateChange(state)}
+          customBurgerIcon={false}
+          customCrossIcon={<FontAwesomeIcon icon={faTimes} />}
+          onStateChange={state => this.handleStateChange(state)}
           outerContainerId={"outer-container"}
           className="bg-dark"
           isOpen={this.state.isOpenMenuPerfil}
@@ -73,9 +76,7 @@ class App extends React.Component {
           </div>
         </BurgerMenu>
         <Headroom id="headRoom" className="bg-dark text-center w-100">
-          <h1 className="w-100 d-inline display-1 text-light">
-            Zainduz
-          </h1>
+          <h1 className="w-100 d-inline display-1 text-light">Zainduz</h1>
           <Avatar
             onClick={() => this.toggleMenu()}
             className="float-right mt-1 align-middle"
@@ -84,6 +85,22 @@ class App extends React.Component {
             src="https://scontent-sea1-1.cdninstagram.com/vp/5eed5e235373aa8292b4ad220a3a388c/5E34F02F/t51.2885-15/e35/51132313_441179589754437_7832601837240715709_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=109&ig_cache_key=MTk4NDQwMjIyODMwNTEzNzYyMA%3D%3D.2"
           />
         </Headroom>
+        <div className="table">
+          <div className="card w-20 m-4" style={{width:"18rem"}}>
+            <div className="card-body">
+              <img src="https://scontent-sea1-1.cdninstagram.com/vp/5eed5e235373aa8292b4ad220a3a388c/5E34F02F/t51.2885-15/e35/51132313_441179589754437_7832601837240715709_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com&_nc_cat=109&ig_cache_key=MTk4NDQwMjIyODMwNTEzNzYyMA%3D%3D.2" class="card-img-top" alt="..." />
+              <h5 className="card-title mt-2">Telmo lizancos - NIÑOOS</h5>
+              <p className="card-text">
+                Hola, soy un puto pederasta y me gusta violar
+              </p>
+              <a href="#" className="btn btn-success">
+               
+                Contactar 
+                <FontAwesomeIcon className="ml-2" icon={faPhone}/>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
