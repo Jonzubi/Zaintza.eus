422636a4e673c60c57bd37553d28578da2700224
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-10-28 19:03:39 +0100

    Se guarda el estado del form de register

--- APP/app_zainduz/src/components/registerForm.js
+++ APP/app_zainduz/src/components/registerForm.js
@@ -6,12 +6,29 @@ class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
-    }
+      txtNombre: "",
+      txtApellido1: "",
+      txtApellido2: "",
+      txtEmail: "",
+      txtSexo: "M",
+      txtContraseña: "",
+      txtDescripcion: ""
+    };
   }
 
-  handleInputChange(){
+  handleInputChange(e) {
     //La idea es recoger el nombre del componente y asignarselo al estado, algo como, this.setState({this.state[name] = e.target.value});
+    var stateId = e.target.id;
+    this.setState({
+      [stateId]: e.target.value
+    });
+  }
+
+  handleSexChange(e) {
+    var sex = e.target.id == "txtSexM" ? "M" : "F";
+    this.setState({
+      txtSexo: sex
+    });
   }
 
   handleRegistrarse() {
@@ -19,18 +36,21 @@ class RegisterForm extends React.Component {
 
     */
     //TODO llamar a la api para insertar
+
+    console.log(this.state);
   }
 
   render() {
     return (
       <div
         className="border border-dark rounded p-5"
-        style={{ margin: "10rem", marginTop:"5rem" }}
+        style={{ margin: "10rem", marginTop: "5rem" }}
       >
         <form>
           <div class="form-group">
             <label for="exampleInputEmail1">Nombre</label>
             <input
+              onChange={this.handleInputChange.bind(this)}
               type="text"
               class="form-control"
               id="txtNombre"
@@ -42,6 +62,7 @@ class RegisterForm extends React.Component {
             <div className="form-group col">
               <label for="exampleInputEmail1">Apellido 1</label>
               <input
+                onChange={this.handleInputChange.bind(this)}
                 type="text"
                 class="form-control"
                 id="txtApellido1"
@@ -52,6 +73,7 @@ class RegisterForm extends React.Component {
             <div className="form-group col">
               <label for="exampleInputEmail1">Apellido 2</label>
               <input
+                onChange={this.handleInputChange.bind(this)}
                 type="text"
                 class="form-control"
                 id="txtApellido2"
@@ -67,6 +89,8 @@ class RegisterForm extends React.Component {
           <div class="form-group row">
             <div className="form-group col-6 text-center">
               <FontAwesomeIcon
+                id="txtSexM"
+                onClick={this.handleSexChange.bind(this)}
                 style={{ cursor: "pointer" }}
                 className="fa-5x"
                 icon={faMale}
@@ -74,6 +98,8 @@ class RegisterForm extends React.Component {
             </div>
             <div className="form-group col-6 text-center">
               <FontAwesomeIcon
+                id="txtSexF"
+                onClick={this.handleSexChange.bind(this)}
                 style={{ cursor: "pointer" }}
                 className="fa-5x"
                 icon={faFemale}
@@ -85,9 +111,10 @@ class RegisterForm extends React.Component {
             <div class="form-group col">
               <label for="exampleInputEmail1">Email</label>
               <input
+                onChange={this.handleInputChange.bind(this)}
                 type="email"
                 class="form-control"
-                id="exampleInputEmail1"
+                id="txtEmail"
                 aria-describedby="emailHelp"
                 placeholder="Introducir email..."
               />
@@ -95,9 +122,10 @@ class RegisterForm extends React.Component {
             <div class="form-group col">
               <label for="exampleInputPassword1">Contraseña</label>
               <input
+                onChange={this.handleInputChange.bind(this)}
                 type="password"
                 class="form-control"
-                id="exampleInputPassword1"
+                id="txtContrasena"
                 placeholder="Introducir contraseña..."
               />
             </div>
@@ -105,10 +133,16 @@ class RegisterForm extends React.Component {
 
           <div class="form-group">
             <label for="comment">Descripcion</label>
-            <textarea class="form-control" rows="5" id="comment" placeholder="Tu descripcion..."></textarea>
+            <textarea
+              onChange={this.handleInputChange.bind(this)}
+              class="form-control"
+              rows="5"
+              id="txtDescripcion"
+              placeholder="Tu descripcion..."
+            ></textarea>
           </div>
           <button
-            onClick={this.handleRegistrarse}
+            onClick={this.handleRegistrarse.bind(this)}
             type="button"
             className="w-100 mt-5 btn btn-success text-center"
           >
