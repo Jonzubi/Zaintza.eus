f14ab08ac14c164992e9ac1d1a8260727c8274cf
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-10-28 18:39:54 +0100

    Haciendo formulario de registro

--- APP/app_zainduz/src/components/registerForm.js
+++ APP/app_zainduz/src/components/registerForm.js
@@ -1,45 +1,121 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMale, faFemale } from "@fortawesome/free-solid-svg-icons";
 
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    }
+  }
+
+  handleInputChange(){
+    //La idea es recoger el nombre del componente y asignarselo al estado, algo como, this.setState({this.state[name] = e.target.value});
+  }
+
+  handleRegistrarse() {
+    /*TODO primero validar todo
+
+    */
+    //TODO llamar a la api para insertar
   }
 
   render() {
     return (
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          <small id="emailHelp" class="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <div
+        className="border border-dark rounded p-5"
+        style={{ margin: "10rem", marginTop:"5rem" }}
+      >
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Nombre</label>
+            <input
+              type="text"
+              class="form-control"
+              id="txtNombre"
+              aria-describedby="txtNombreHelp"
+              placeholder="Introducir nombre..."
+            />
+          </div>
+          <div class="form-group row">
+            <div className="form-group col">
+              <label for="exampleInputEmail1">Apellido 1</label>
+              <input
+                type="text"
+                class="form-control"
+                id="txtApellido1"
+                aria-describedby="txtNombreHelp"
+                placeholder="Introducir apellido 1..."
+              />
+            </div>
+            <div className="form-group col">
+              <label for="exampleInputEmail1">Apellido 2</label>
+              <input
+                type="text"
+                class="form-control"
+                id="txtApellido2"
+                aria-describedby="txtNombreHelp"
+                placeholder="Introducir apellido 2..."
+              />
+            </div>
+          </div>
+          {/****************************************
+          Meter fecha nacimiento aqui en un futuro        
+         *****************************************/}
+
+          <div class="form-group row">
+            <div className="form-group col-6 text-center">
+              <FontAwesomeIcon
+                style={{ cursor: "pointer" }}
+                className="fa-5x"
+                icon={faMale}
+              />
+            </div>
+            <div className="form-group col-6 text-center">
+              <FontAwesomeIcon
+                style={{ cursor: "pointer" }}
+                className="fa-5x"
+                icon={faFemale}
+              />
+            </div>
+          </div>
+
+          <div className="form-group row">
+            <div class="form-group col">
+              <label for="exampleInputEmail1">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Introducir email..."
+              />
+            </div>
+            <div class="form-group col">
+              <label for="exampleInputPassword1">Contraseña</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Introducir contraseña..."
+              />
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="comment">Descripcion</label>
+            <textarea class="form-control" rows="5" id="comment" placeholder="Tu descripcion..."></textarea>
+          </div>
+          <button
+            onClick={this.handleRegistrarse}
+            type="button"
+            className="w-100 mt-5 btn btn-success text-center"
+          >
+            Registrarse
+          </button>
+        </form>
+      </div>
     );
   }
 }
