41afcab4f59594c958ee702954036a45e3f454ff
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-11-19 19:20:01 +0100

    Iconos de que publico cuida implementado

--- APP/app_zainduz/src/components/tablaCuidadores.js
+++ APP/app_zainduz/src/components/tablaCuidadores.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Axios from "axios";
 import ipMaquina from "../util/ipMaquinaAPI";
 
@@ -30,7 +30,12 @@ class Tabla extends React.Component {
                   <td className="col-3 border-top-0">
                     <div className="card w-20 m-4" style={{ width: "18rem" }}>
                       <img
-                        src={"http://" + ipMaquina + ":3001/image/" + cuidador.direcFotoContacto}
+                        src={
+                          "http://" +
+                          ipMaquina +
+                          ":3001/image/" +
+                          cuidador.direcFotoContacto
+                        }
                         class="card-img-top"
                         alt="..."
                       />
@@ -41,6 +46,57 @@ class Tabla extends React.Component {
                         <p className="card-text">
                           {cuidador.descripcion || " a"}
                         </p>
+                      </div>
+                      <div className="card-body">
+                        <div className="row text-muted card-title">
+                          <div className="col text-center">Niños</div>
+                          <div className="col text-center">3º edad</div>
+                          <div className="col text-center">N.E</div>
+                        </div>
+
+                        <div className="row">
+                          <div className="col text-center">
+                            {cuidador.publicoDisponible.nino ? (
+                              <FontAwesomeIcon
+                                className="text-success"
+                                icon={faCheck}
+                              />
+                            ) : (
+                              <FontAwesomeIcon
+                                className="text-danger"
+                                icon={faTimes}
+                              />
+                            )}
+                          </div>
+                          <div className="col text-center">
+                            {cuidador.publicoDisponible.terceraEdad ? (
+                              <FontAwesomeIcon
+                                className="text-success"
+                                icon={faCheck}
+                              />
+                            ) : (
+                              <FontAwesomeIcon
+                                className="text-danger"
+                                icon={faTimes}
+                              />
+                            )}
+                          </div>
+                          <div className="col text-center">
+                            {cuidador.publicoDisponible.necesidadEspecial ? (
+                              <FontAwesomeIcon
+                                className="text-success"
+                                icon={faCheck}
+                              />
+                            ) : (
+                              <FontAwesomeIcon
+                                className="text-danger"
+                                icon={faTimes}
+                              />
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="card-body card-footer">
                         <a href="#" className="mr-0 w-100 btn btn-success">
                           Contactar
                           <FontAwesomeIcon className="ml-1" icon={faPhone} />
