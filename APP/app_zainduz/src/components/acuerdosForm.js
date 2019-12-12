cd00cd4c2e2dea1861605c287aa967937e760bf0
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2019-12-12 18:48:22 +0100

    Añadiendo la informacion de la otra parte del acuerdo al json

--- APP/app_zainduz/src/components/acuerdosForm.js
+++ APP/app_zainduz/src/components/acuerdosForm.js
@@ -9,7 +9,8 @@ import { t } from "../util/funciones";
 
 const mapStateToProps = state => {
   return {
-    idPerfil: state.user._id
+    idPerfil: state.user._id,
+    tipoUsuario: state.user.tipoUsuario
   };
 };
 
@@ -25,14 +26,32 @@ class AcuerdosForm extends React.Component {
         {
           /* En estas lineas inicializo un array donde guardara el estado de los collapse que van a se los acuerdos. Por cada acuerdo guardara un false al inicio */
         }
+        //buscarUsuOrCuid =>> En esta variable guardo si el usuario iniciado es cliente o cuidador para asi si es cuidador 
+        //buscar cliente en el acuerdo y viceversa, ESTO ME DARA LA INFORMACION DE LA OTRA PARTE DEL ACUERDO
+        let buscarUsuOrCuid = this.props.tipoUsuario == "Z" ? "idCliente" : "idCuidador";
         let countAcuerdos = resultado.data.length;
         let auxAcuerdosCollapseState = [];
+        let jsonAcuerdos=[];
         for (let i = 0; i < countAcuerdos; i++) {
+          console.log("http://" + ipMaquina + ":3001/" + buscarUsuOrCuid == "idCliente" ? "cliente" : "cuidador" + "/" + resultado.data[i][buscarUsuOrCuid]);
+          continue;
+          axios.get("http://" + ipMaquina + ":3001/" + buscarUsuOrCuid == "idCliente" ? "cliente" : "cuidador" + "/" + resultado.data[i][buscarUsuOrCuid])
+          .then(otro => {
+            resultado.data[i] = Object.assign({}, resultado.data[i], otro);
+            jsonAcuerdos.push(resultado.data[i]);
+            if(i == countAcuerdos - 1){
+              this.setState({
+                jsonAcuerdos:jsonAcuerdos
+              })
+            }
+          })
+          .catch(err=> {
+            console.log(err);
+          });
           auxAcuerdosCollapseState.push(false);
         }
 
         this.setState({
-          jsonAcuerdos: resultado.data,
           acuerdosCollapseState: auxAcuerdosCollapseState
         });
       })
