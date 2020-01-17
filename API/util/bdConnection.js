09f06723fbcf939aff5dd512517d59dc4296cc3a
Jon Zubizarreta <jonzubi97@gmail.com>
2020-01-18 00:15:41 +0100

    Switch to AWS

--- API/util/bdConnection.js
+++ API/util/bdConnection.js
@@ -2,12 +2,12 @@ let mongoose = require("mongoose");
 const pwdAtlas = require("./mongoDBAtlasPWD");
 const ipMaquina = require("./ipMaquina");
 //Esta es para el Atlas
-//const uri =
-//  "mongodb+srv://admin:" +
-//  pwdAtlas +
-//  "@jonzubicloud-7eg52.azure.mongodb.net/zaintza?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://admin:" +
+  pwdAtlas +
+  "@jonzubicloud-7eg52.azure.mongodb.net/zaintza?retryWrites=true&w=majority";
 //Esta es la local
-const uri = 'mongodb://' + ipMaquina + ':27017/zaintza';
+//const uri = 'mongodb://' + ipMaquina + ':27017/zaintza';
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
