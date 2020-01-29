41e3cc70e1f41f0729e49c788e8ef55d1f091c23
Jon <jonzubi97@gmail.com>
2020-01-29 21:40:23 +0100

    Empezando con tablaClientes o tablaAnuncios

--- APP/app_zainduz/src/components/tablaClientes.js
+++ APP/app_zainduz/src/components/tablaClientes.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 class Tabla extends React.Component {
   render() {
     return (
-    <h1>Aqui van los anuncios</h1>
+      <h1>Anuncios</h1>
     );
   }
 }
