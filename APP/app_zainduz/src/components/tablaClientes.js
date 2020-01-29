a67aa01db5ade960a06fb5179e45709a332d2bd2
Jon <jonzubi97@gmail.com>
2020-01-29 21:56:06 +0100

    Hecho un boton de añadir provisional

--- APP/app_zainduz/src/components/tablaClientes.js
+++ APP/app_zainduz/src/components/tablaClientes.js
@@ -1,13 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import { changeFormContent } from "../redux/actions/app";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
-class Tabla extends React.Component {
+const mapStateToProps = state => {
+  return {
+    tipoUsuario: state.user.tipoUsuario
+  };
+};
+
+const mapDispatcToProps = dispatch => {
+  return {
+    changeFormContent: form => dispatch(changeFormContent(form))
+  };
+};
+
+class TablaClientes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  botonAddAnuncio = () => {
+    const { tipoUsuario } = this.props;
+
+    if (tipoUsuario === "Cliente") {
+      return (
+        <div className="w-100 float-bottom btn btn-success">Añadir anuncio</div>
+      );
+    }
+
+    return null;
+  };
+
   render() {
-    return (
-      <h1>Anuncios</h1>
-    );
+    return this.botonAddAnuncio();
   }
 }
 
-export default Tabla;
+export default connect(mapStateToProps, mapDispatcToProps)(TablaClientes);
