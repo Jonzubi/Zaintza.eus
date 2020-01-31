996f21b10358ccf11af3ee5d6f9873115b774ccb
Jon <jonzubi97@gmail.com>
2020-02-01 00:27:59 +0100

    Componentes formAnuncio construido, toca programarlo

--- APP/app_zainduz/src/components/formAnuncio.js
+++ APP/app_zainduz/src/components/formAnuncio.js
@@ -3,6 +3,18 @@ import cogoToast from "cogo-toast";
 import { trans } from "../util/funciones";
 import ImageUploader from "react-images-upload";
 import i18next from "i18next";
+import TimeInput from "react-time-input";
+import AutoSuggest from "react-autosuggest";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMale,
+  faFemale,
+  faPlusCircle,
+  faMinusCircle,
+  faPlusSquare
+} from "@fortawesome/free-solid-svg-icons";
+import municipios from "../util/municipos";
+import ipMaquina from "../util/ipMaquinaAPI";
 
 class FormAnuncio extends React.Component {
   constructor(props) {
@@ -12,6 +24,10 @@ class FormAnuncio extends React.Component {
       imgContact: null,
       txtTitulo: "",
       txtDescripcion: "",
+      auxAddPueblo: "",
+      suggestionsPueblos: [],
+      diasDisponible: [],
+      ubicaciones: [],
       error: false
     };
 
@@ -19,6 +35,21 @@ class FormAnuncio extends React.Component {
     this.onCrop = this.onCrop.bind(this);
     this.onChangeContactImg = this.onChangeContactImg.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAuxAddPuebloChange = this.handleAuxAddPuebloChange.bind(this);
+    this.getSuggestions = this.getSuggestions.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(
+      this
+    );
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(
+      this
+    );
+    this.handleAddPueblo = this.handleAddPueblo.bind(this);
+    this.handleRemovePueblo = this.handleRemovePueblo.bind(this);
+    this.addDiasDisponible = this.addDiasDisponible.bind(this);
+    this.removeDiasDisponible = this.removeDiasDisponible.bind(this);
+    this.handleDiasDisponibleChange = this.handleDiasDisponibleChange.bind(
+      this
+    );
   }
 
   onClose() {
@@ -54,7 +85,166 @@ class FormAnuncio extends React.Component {
     });
   }
 
+  handleAuxAddPuebloChange(e, { newValue }) {
+    this.setState({
+      auxAddPueblo: newValue
+    });
+  }
+
+  escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  getSuggestions(value) {
+    const escapedValue = this.escapeRegexCharacters(value.trim());
+
+    if (escapedValue === "") {
+      return [];
+    }
+
+    const regex = new RegExp("^" + escapedValue, "i");
+
+    return municipios.filter(pueblo => regex.test(pueblo));
+  }
+
+  getSuggestionValue(suggestion) {
+    return suggestion;
+  }
+
+  renderSuggestion(suggestion) {
+    return <span>{suggestion}</span>;
+  }
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestionsPueblos: this.getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestionsPueblos: []
+    });
+  };
+
+  handleAddPueblo() {
+    let pueblo = this.state.auxAddPueblo;
+    if (pueblo == "") return;
+
+    if (!municipios.includes(pueblo)) {
+      cogoToast.error(
+        <h5>
+          {pueblo} {trans("registerFormCuidadores.errorPuebloNoExiste")}
+        </h5>
+      );
+      return;
+    }
+
+    for (var clave in this.state.ubicaciones) {
+      if (this.state.ubicaciones[clave] == pueblo) {
+        cogoToast.error(
+          <h5>
+            {pueblo} {trans("registerFormCuidadores.errorPuebloRepetido")}
+          </h5>
+        );
+        return;
+      }
+    }
+    this.state.ubicaciones.push(pueblo);
+    this.setState({
+      ubicaciones: this.state.ubicaciones,
+      auxAddPueblo: ""
+    });
+  }
+
+  handleRemovePueblo() {
+    this.setState({
+      ubicaciones:
+        typeof this.state.ubicaciones.pop() != "undefined"
+          ? this.state.ubicaciones
+          : []
+    });
+  }
+
+  addDiasDisponible() {
+    let auxDiasDisponible = this.state.diasDisponible;
+    auxDiasDisponible.push({
+      dia: 0,
+      horaInicio: "00:00",
+      horaFin: "00:00"
+    });
+
+    this.setState({
+      diasDisponible: auxDiasDisponible
+    });
+  }
+
+  removeDiasDisponible() {
+    this.setState({
+      diasDisponible:
+        typeof this.state.diasDisponible.pop() != "undefined"
+          ? this.state.diasDisponible
+          : []
+    });
+  }
+
+  handleDiasDisponibleChange(e, indice) {
+    if (typeof indice == "undefined") {
+      //Significa que lo que se ha cambiado es el combo de los dias
+      var origen = e.target;
+      var indice = parseInt(origen.id.substr(origen.id.length - 1));
+      var valor = origen.value;
+
+      let auxDiasDisponible = this.state.diasDisponible;
+      auxDiasDisponible[indice]["dia"] = valor;
+
+      this.setState({
+        diasDisponible: auxDiasDisponible
+      });
+    } else {
+      //Significa que ha cambiado la hora, no se sabe si inicio o fin, eso esta en "indice"
+      let atributo = indice.substr(0, indice.length - 1);
+      indice = indice.substr(indice.length - 1);
+
+      let auxDiasDisponible = this.state.diasDisponible;
+      auxDiasDisponible[indice][atributo] = e;
+
+      this.setState({
+        diasDisponible: auxDiasDisponible
+      });
+    }
+  }
+
   render() {
+    const { auxAddPueblo, error, suggestionsPueblos } = this.state;
+    const onChangeSuggestion = this.handleAuxAddPuebloChange;
+    const classSuggestion = error
+      ? "border border-danger form-control d-inline w-75"
+      : "form-control d-inline w-100";
+    const autoSuggestProps = {
+      onChange: onChangeSuggestion,
+      placeholder: "Introduce el pueblo...",
+      value: auxAddPueblo,
+      className: classSuggestion
+    };
+
+    const suggestionTheme = {
+      container: "react-autosuggest__container",
+      containerOpen: "react-autosuggest__container--open",
+      input: "react-autosuggest__input",
+      inputOpen: "react-autosuggest__input--open",
+      inputFocused: "react-autosuggest__input--focused",
+      suggestionsContainer: "list-group",
+      suggestionsContainerOpen:
+        "react-autosuggest__suggestions-container--open",
+      suggestionsList: "list-group",
+      suggestion: "list-group-item",
+      suggestionFirst: "list-group-item",
+      suggestionHighlighted: "bg-success text-light list-group-item",
+      sectionContainer: "react-autosuggest__section-container",
+      sectionContainerFirst: "react-autosuggest__section-container--first",
+      sectionTitle: "react-autosuggest__section-title"
+    };
     return (
       <div className="p-5">
         <div className="form-group d-flex justify-content-center position-relative">
@@ -129,7 +319,164 @@ class FormAnuncio extends React.Component {
             value={this.state.txtDescripcion}
           ></textarea>
         </div>
-        
+        <div className="form-group row">
+          <div className="form-group col">
+            {/* Insertar dias disponibles aqui */}
+            <label className="w-100 text-center lead">
+              {trans("registerFormCuidadores.diasDisponible")}:
+            </label>
+            <br />
+            <div className="w-100 mt-2" id="diasDisponible">
+              {/* Aqui iran los dias dinamicamente */}
+              {this.state.diasDisponible.map((objDia, indice) => {
+                return (
+                  <div
+                    className="col-6 mx-auto text-center"
+                    id={"diaDisponible" + indice}
+                  >
+                    <div className="form-control mt-4 w-100">
+                      <select
+                        value={this.state.diasDisponible[indice].dia}
+                        onChange={this.handleDiasDisponibleChange}
+                        className="d-inline"
+                        id={"dia" + indice}
+                      >
+                        <option>Aukeratu eguna</option>
+                        <option value="1">Astelehena</option>
+                        <option value="2">Asteartea</option>
+                        <option value="3">Asteazkena</option>
+                        <option value="4">Osteguna</option>
+                        <option value="5">Ostirala</option>
+                        <option value="6">Larunbata</option>
+                        <option value="7">Igandea</option>
+                      </select>
+                      <br />
+                      <br />
+                      <b>{trans("registerFormCuidadores.horaInicio")} :</b>
+                      <TimeInput
+                        onTimeChange={valor => {
+                          this.handleDiasDisponibleChange(
+                            valor,
+                            "horaInicio" + indice
+                          );
+                        }}
+                        id={"horaInicio" + indice}
+                        initTime={
+                          this.state.diasDisponible[indice].horaInicio !=
+                          "00:00"
+                            ? this.state.diasDisponible[indice].horaInicio
+                            : "00:00"
+                        }
+                        className="mt-1 text-center d-inline form-control"
+                      />
+                      <br />
+                      <b>{trans("registerFormCuidadores.horaFin")} :</b>
+                      <TimeInput
+                        onTimeChange={valor => {
+                          this.handleDiasDisponibleChange(
+                            valor,
+                            "horaFin" + indice
+                          );
+                        }}
+                        id={"horaFin" + indice}
+                        initTime={
+                          this.state.diasDisponible[indice].horaFin != "00:00"
+                            ? this.state.diasDisponible[indice].horaFin
+                            : "00:00"
+                        }
+                        className="mt-1 text-center d-inline form-control"
+                      />
+                      <br />
+                      <br />
+                    </div>
+                  </div>
+                );
+              })}
+              <div id="botonesDiasDisponible" className="w-100 mt-2">
+                {this.state.diasDisponible.length > 0 ? (
+                  <a
+                    onClick={this.removeDiasDisponible}
+                    className="btn btn-danger float-left text-light"
+                  >
+                    {trans("registerFormCuidadores.eliminarDia")}{" "}
+                    <FontAwesomeIcon icon={faMinusCircle} />
+                  </a>
+                ) : (
+                  ""
+                )}
+                <a
+                  onClick={this.addDiasDisponible}
+                  className="btn btn-success float-right text-light"
+                >
+                  {trans("registerFormCuidadores.anadir")}{" "}
+                  <FontAwesomeIcon icon={faPlusCircle} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="form-group col">
+            {/* Insertar ubicaciones disponibles aqui */}
+            <label htmlFor="txtAddPueblos" className="w-100 text-center lead">
+              {trans("registerFormCuidadores.pueblosDisponible")}:
+            </label>{" "}
+            (<span className="text-danger font-weight-bold">*</span>)
+            <div class="form-group mt-2">
+              <AutoSuggest
+                suggestions={suggestionsPueblos}
+                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                onSuggestionSelected={this.handleAddPueblo}
+                getSuggestionValue={this.getSuggestionValue}
+                renderSuggestion={this.renderSuggestion}
+                inputProps={autoSuggestProps}
+                theme={suggestionTheme}
+                id="txtAddPueblos"
+              />
+              {this.state.ubicaciones.length > 0 ? (
+                <h5 className="mt-2 lead">
+                  {trans("registerFormCuidadores.pueblosSeleccionados")}:
+                </h5>
+              ) : (
+                ""
+              )}
+
+              <ul className="list-group">
+                {this.state.ubicaciones.map(pueblo => {
+                  return <li className="list-group-item">{pueblo}</li>;
+                })}
+              </ul>
+              {this.state.ubicaciones.length > 0 ? (
+                <a
+                  onClick={this.handleRemovePueblo}
+                  className="mt-4 btn btn-danger float-right text-light"
+                >
+                  {trans("registerFormCuidadores.eliminarPueblo")}{" "}
+                  <FontAwesomeIcon icon={faMinusCircle} />
+                </a>
+              ) : (
+                ""
+              )}
+            </div>
+            <br />
+          </div>
+        </div>
+        <div id="loaderOrButton" className="w-100 mt-5 text-center">
+            {this.state.isLoading ? (
+              <img
+                src={"http://" + ipMaquina + ":3001/api/image/loadGif"}
+                height={50}
+                width={50}
+              />
+            ) : (
+              <button
+                onClick={() => cogoToast.info("Handle subir anuncio")}
+                type="button"
+                className="w-100 btn btn-success "
+              >
+                {trans("registerFormCuidadores.registrarse")}
+              </button>
+            )}
+          </div>
       </div>
     );
   }
