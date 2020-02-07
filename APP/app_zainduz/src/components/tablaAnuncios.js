9a37fe96c31cada2198baba6fee50a98bb2c2d1b
Jon Zubizarreta Iturria <jonzubizarreta@segundo.edu>
2020-02-07 18:56:11 +0100

    tablaAnuncios mostrando anuncios, todavia incompleto

--- APP/app_zainduz/src/components/tablaAnuncios.js
+++ APP/app_zainduz/src/components/tablaAnuncios.js
@@ -2,9 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import { trans } from "../util/funciones";
 import { changeFormContent } from "../redux/actions/app";
-import axios from 'axios';
-import cogoToast from 'cogo-toast';
+import axios from "axios";
+import cogoToast from "cogo-toast";
 import ipMaquina from "../util/ipMaquinaAPI";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhoneAlt,
+  faCalendar,
+  faHome
+} from "@fortawesome/free-solid-svg-icons";
 
 const mapStateToProps = state => {
   return {
@@ -22,20 +28,22 @@ class TablaAnuncios extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      jsonAnuncios: {}
+      jsonAnuncios: []
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios
-      .get('http://' + ipMaquina + ':3001/api/procedures/getAnunciosConPerfil')
+      .get("http://" + ipMaquina + ":3001/api/procedures/getAnunciosConPerfil")
       .then(res => {
         this.setState({
           jsonAnuncios: res.data
         });
       })
       .catch(err => {
-        cogoToast.error(<h5>{trans("notificaciones.servidorNoDisponible")}</h5>);
+        cogoToast.error(
+          <h5>{trans("notificaciones.servidorNoDisponible")}</h5>
+        );
       });
   }
 
@@ -44,10 +52,12 @@ class TablaAnuncios extends React.Component {
 
     if (tipoUsuario === "Cliente") {
       return (
-        <div 
+        <div
           className="d-flex justify-content-center btn btn-success"
-          onClick= {() => changeFormContent('formAnuncio')}
-      >{trans('tablaAnuncios.addAnuncio')}</div>
+          onClick={() => changeFormContent("formAnuncio")}
+        >
+          {trans("tablaAnuncios.addAnuncio")}
+        </div>
       );
     }
 
@@ -57,7 +67,56 @@ class TablaAnuncios extends React.Component {
   render() {
     const { jsonAnuncios } = this.state;
     console.log(jsonAnuncios);
-    return this.botonAddAnuncio();
+    return (
+      <div className="p-5">
+        {this.botonAddAnuncio()}
+
+        {jsonAnuncios.map(anuncio => {
+          return (
+            <div className="row card-header mt-2 mb-2">
+              <div style={{ width: "300px" }}>
+                <img
+                  className="img-responsive"
+                  src={
+                    "http://" +
+                    ipMaquina +
+                    ":3001/api/image/" +
+                    anuncio.direcFoto
+                  }
+                  style={{
+                    minHeight: "300px",
+                    maxHeight: "300px",
+                    height: "auto"
+                  }}
+                />
+                <div className="align-center text-center">
+                  <FontAwesomeIcon className="text-success" icon={faHome} />{" "}
+                  <span className="font-weight-bold">{anuncio.pueblo}</span>
+                </div>
+              </div>
+
+              <div className="col">
+                <h3>{anuncio.titulo}</h3>
+                <hr />
+                <h5>{anuncio.descripcion}</h5>
+                <div className="row">
+                  <FontAwesomeIcon
+                    size={"2x"}
+                    className="col text-success"
+                    icon={faPhoneAlt}
+                  />
+                  <FontAwesomeIcon
+                    size={"2x"}
+                    className="col text-success"
+                    icon={faCalendar}
+                  />
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
