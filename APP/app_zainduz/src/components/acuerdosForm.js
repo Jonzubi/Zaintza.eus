4bf928a31d591a9a1d2b32560b0b1336bc0b252a
Jon <jonzubi97@gmail.com>
2020-03-07 18:17:15 +0100

    Cuando se termina un acuerdo por AcuerdoForm se le notifica a la otra persona del acuerdo

--- APP/app_zainduz/src/components/acuerdosForm.js
+++ APP/app_zainduz/src/components/acuerdosForm.js
@@ -15,6 +15,7 @@ import ipMaquina from "../util/ipMaquinaAPI";
 import { trans, arrayOfFalses, getTodayDate } from "../util/funciones";
 import Avatar from "react-avatar";
 import cogoToast from "cogo-toast";
+import SocketContext from "../socketio/socket-context";
 
 const mapStateToProps = state => {
   return {
@@ -29,22 +30,26 @@ class AcuerdosForm extends React.Component {
     //buscarUsuOrCuid =>> En esta variable guardo si el usuario iniciado es cliente o cuidador para asi si es cuidador
     //buscar cliente en el acuerdo y viceversa, ESTO ME DARA LA INFORMACION DE LA OTRA PARTE DEL ACUERDO
     const { idPerfil, tipoUsuario } = this.props;
-    
-    axios.get("http://" + ipMaquina + ":3001/api/procedures/getAcuerdosConUsuarios", {
-      params: {
-        tipoUsuario: tipoUsuario,
-        idPerfil: idPerfil
-      }      
-    })
-    .then(resultado => {
-      this.setState({
-        jsonAcuerdos: resultado.data,
-        acuerdosCollapseState: arrayOfFalses(resultado.data.length)
+
+    axios
+      .get(
+        "http://" + ipMaquina + ":3001/api/procedures/getAcuerdosConUsuarios",
+        {
+          params: {
+            tipoUsuario: tipoUsuario,
+            idPerfil: idPerfil
+          }
+        }
+      )
+      .then(resultado => {
+        this.setState({
+          jsonAcuerdos: resultado.data,
+          acuerdosCollapseState: arrayOfFalses(resultado.data.length)
+        });
+      })
+      .catch(err => {
+        console.log(err);
       });
-    })
-    .catch(err => {
-      console.log(err);
-    });
   }
 
   constructor(props) {
@@ -90,41 +95,52 @@ class AcuerdosForm extends React.Component {
     }
   }
 
-  async handleTerminarAcuerdo(acuerdo, indice) {
+  async handleTerminarAcuerdo(acuerdo, indice, socket) {
     if (acuerdo.estadoAcuerdo == 2) {
       return;
     }
-    
+
     let today = getTodayDate();
     const objToday = new Date();
 
-    await axios.patch("http://" + ipMaquina + ":3001/api/acuerdo/" + acuerdo._id, {
-      estadoAcuerdo: 2,
-      dateFinAcuerdo: today
-    });
+    await axios.patch(
+      "http://" + ipMaquina + ":3001/api/acuerdo/" + acuerdo._id,
+      {
+        estadoAcuerdo: 2,
+        dateFinAcuerdo: today
+      }
+    );
     //Ahora se quiere notificar a la otra parte del acuerdo de la finalizacion del acuerdo
     let buscarUsuOrCuid =
       this.props.tipoUsuario == "Cliente" ? "idCuidador" : "idCliente";
     const idElOtro = acuerdo[buscarUsuOrCuid];
-    let elOtroUsu = await axios.get("http://" + ipMaquina + ":3001/api/usuario", {
-      params: {
-        filtros: {
-          idPerfil: idElOtro
+    let elOtroUsu = await axios.get(
+      "http://" + ipMaquina + ":3001/api/usuario",
+      {
+        params: {
+          filtros: {
+            idPerfil: idElOtro
+          }
         }
       }
-    });
+    );
     const notificacionData = {
       idUsuario: elOtroUsu.data[0]._id,
       idRemitente: this.props.idUsuario,
       tipoNotificacion: "AcuerdoGestionado",
       valorGestion: false,
       visto: false,
-      dateEnvioNotificacion: today + " " + objToday.getHours() + ":" + objToday.getMinutes()
+      dateEnvioNotificacion:
+        today + " " + objToday.getHours() + ":" + objToday.getMinutes()
     };
     await axios.post(
       "http://" + ipMaquina + ":3001/api/notificacion",
       notificacionData
     );
+
+    socket.emit('notify', {
+      idUsuario: elOtroUsu.data[0]._id
+    });
 
     let auxJsonAcuerdos = this.state.jsonAcuerdos;
     auxJsonAcuerdos[indice].estadoAcuerdo = 2;
@@ -139,177 +155,186 @@ class AcuerdosForm extends React.Component {
   }
 
   render() {
-    const laOtraPersona = this.props.tipoUsuario != "Cuidador" ? "idCuidador" : "idCliente";
+    const laOtraPersona =
+      this.props.tipoUsuario != "Cuidador" ? "idCuidador" : "idCliente";
     return (
-      <div className="p-5 h-100">
-        {this.state.jsonAcuerdos.length != 0 ? (
-          this.state.jsonAcuerdos.map((acuerdo, indice) => {
-            return (
-              <div className="w-100 card">
-                <div className="card-header">
-                  <div className="row">
-                    <div className="col-10 text-center">
-                      <div className="d-flex align-items-center">
-                        <Avatar
-                          size={50}
-                          className=""
-                          name={acuerdo[laOtraPersona].nombre}
-                          src={
-                            "http://" +
-                            ipMaquina +
-                            ":3001/api/image/" +
-                            acuerdo[laOtraPersona].direcFoto
-                          }
-                        />
-                        <div className="ml-3">
-                          <span className="font-weight-bold">
-                            {acuerdo[laOtraPersona].nombre +
-                              " " +
-                              acuerdo[laOtraPersona].apellido1}
-                          </span>{" "}
-                          <span>
-                            {acuerdo.estadoAcuerdo == 0
-                              ? trans("acuerdosForm.esperandoAcuerdo")
-                              : acuerdo.estadoAcuerdo == 1
-                              ? trans("acuerdosForm.aceptadoAcuerdo")
-                              : trans("acuerdosForm.rechazadoAcuerdo")}
-                          </span>
+      <SocketContext.Consumer>
+        {socket => (
+          <div className="p-5 h-100">
+            {this.state.jsonAcuerdos.length != 0 ? (
+              this.state.jsonAcuerdos.map((acuerdo, indice) => {
+                return (
+                  <div className="w-100 card">
+                    <div className="card-header">
+                      <div className="row">
+                        <div className="col-10 text-center">
+                          <div className="d-flex align-items-center">
+                            <Avatar
+                              size={50}
+                              className=""
+                              name={acuerdo[laOtraPersona].nombre}
+                              src={
+                                "http://" +
+                                ipMaquina +
+                                ":3001/api/image/" +
+                                acuerdo[laOtraPersona].direcFoto
+                              }
+                            />
+                            <div className="ml-3">
+                              <span className="font-weight-bold">
+                                {acuerdo[laOtraPersona].nombre +
+                                  " " +
+                                  acuerdo[laOtraPersona].apellido1}
+                              </span>{" "}
+                              <span>
+                                {acuerdo.estadoAcuerdo == 0
+                                  ? trans("acuerdosForm.esperandoAcuerdo")
+                                  : acuerdo.estadoAcuerdo == 1
+                                  ? trans("acuerdosForm.aceptadoAcuerdo")
+                                  : trans("acuerdosForm.rechazadoAcuerdo")}
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-1 text-center my-auto">
+                          {acuerdo.estadoAcuerdo == 0 ? (
+                            <OverlayTrigger
+                              key="top"
+                              placement="top"
+                              overlay={
+                                <Tooltip>
+                                  {trans("acuerdosForm.estadoPendiente")}
+                                </Tooltip>
+                              }
+                            >
+                              <FontAwesomeIcon
+                                className="text-secondary"
+                                icon={faCircle}
+                              />
+                            </OverlayTrigger>
+                          ) : acuerdo.estadoAcuerdo == 1 ? (
+                            <OverlayTrigger
+                              key="top"
+                              placement="top"
+                              overlay={
+                                <Tooltip>
+                                  {trans("acuerdosForm.estadoAceptado")}
+                                </Tooltip>
+                              }
+                            >
+                              <FontAwesomeIcon
+                                className="text-success"
+                                icon={faCircle}
+                              />
+                            </OverlayTrigger>
+                          ) : (
+                            <OverlayTrigger
+                              key="top"
+                              placement="top"
+                              overlay={
+                                <Tooltip>
+                                  {trans("acuerdosForm.estadoRechazado")}
+                                </Tooltip>
+                              }
+                            >
+                              <FontAwesomeIcon
+                                className="text-danger"
+                                icon={faCircle}
+                              />
+                            </OverlayTrigger>
+                          )}
+                        </div>
+                        <div className="col-1 text-center my-auto">
+                          <FontAwesomeIcon
+                            style={{ cursor: "pointer" }}
+                            size="2x"
+                            icon={faCaretDown}
+                            className=""
+                            onClick={() =>
+                              this.handleToogleCollapseAcuerdo(indice)
+                            }
+                          />
                         </div>
                       </div>
                     </div>
-                    <div className="col-1 text-center my-auto">
-                      {acuerdo.estadoAcuerdo == 0 ? (
-                        <OverlayTrigger
-                          key="top"
-                          placement="top"
-                          overlay={
-                            <Tooltip>
-                              {trans("acuerdosForm.estadoPendiente")}
-                            </Tooltip>
-                          }
-                        >
-                          <FontAwesomeIcon
-                            className="text-secondary"
-                            icon={faCircle}
-                          />
-                        </OverlayTrigger>
-                      ) : acuerdo.estadoAcuerdo == 1 ? (
-                        <OverlayTrigger
-                          key="top"
-                          placement="top"
-                          overlay={
-                            <Tooltip>
-                              {trans("acuerdosForm.estadoAceptado")}
-                            </Tooltip>
-                          }
-                        >
-                          <FontAwesomeIcon
-                            className="text-success"
-                            icon={faCircle}
-                          />
-                        </OverlayTrigger>
-                      ) : (
-                        <OverlayTrigger
-                          key="top"
-                          placement="top"
-                          overlay={
-                            <Tooltip>
-                              {trans("acuerdosForm.estadoRechazado")}
-                            </Tooltip>
-                          }
-                        >
-                          <FontAwesomeIcon
-                            className="text-danger"
-                            icon={faCircle}
-                          />
-                        </OverlayTrigger>
-                      )}
-                    </div>
-                    <div className="col-1 text-center my-auto">
-                      <FontAwesomeIcon
-                        style={{ cursor: "pointer" }}
-                        size="2x"
-                        icon={faCaretDown}
-                        className=""
-                        onClick={() => this.handleToogleCollapseAcuerdo(indice)}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <Collapse
-                  className="card-body"
-                  isOpened={this.state.acuerdosCollapseState[indice]}
-                >
-                  <div>
-                    <span className="display-4">{acuerdo.tituloAcuerdo}</span>
-                    <hr />
-                    <div className="p-5 font-weight-bold text-center">
-                      {acuerdo.descripcionAcuerdo}
-                    </div>
-                    <div className="row">
-                      <div className="col-6 text-center">
-                        <span>{trans("notificacionesForm.pueblos")}</span>
+                    <Collapse
+                      className="card-body"
+                      isOpened={this.state.acuerdosCollapseState[indice]}
+                    >
+                      <div>
+                        <span className="display-4">
+                          {acuerdo.tituloAcuerdo}
+                        </span>
                         <hr />
-                        <ul className="list-group">
-                          {typeof acuerdo.pueblo.map != "undefined"
-                            ? acuerdo.pueblo.map(pueblo => {
-                                return (
-                                  <li className="list-group-item font-weight-bold">
-                                    {pueblo}
-                                  </li>
-                                );
-                              })
-                            : null}
-                        </ul>
+                        <div className="p-5 font-weight-bold text-center">
+                          {acuerdo.descripcionAcuerdo}
+                        </div>
+                        <div className="row">
+                          <div className="col-6 text-center">
+                            <span>{trans("notificacionesForm.pueblos")}</span>
+                            <hr />
+                            <ul className="list-group">
+                              {typeof acuerdo.pueblo.map != "undefined"
+                                ? acuerdo.pueblo.map(pueblo => {
+                                    return (
+                                      <li className="list-group-item font-weight-bold">
+                                        {pueblo}
+                                      </li>
+                                    );
+                                  })
+                                : null}
+                            </ul>
+                          </div>
+                          <div className="col-6 text-center">
+                            <span>{trans("notificacionesForm.dias")}</span>
+                            <hr />
+                            <ul className="list-group">
+                              {typeof acuerdo.diasAcordados.map != "undefined"
+                                ? acuerdo.diasAcordados.map(dia => {
+                                    return (
+                                      <li className="list-group-item">
+                                        <span className="font-weight-bold">
+                                          {this.traducirDia(dia.dia) + ": "}
+                                        </span>
+                                        <span>
+                                          {dia.horaInicio + " - " + dia.horaFin}
+                                        </span>
+                                      </li>
+                                    );
+                                  })
+                                : null}
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="row ml-0 mr-0 mt-5">
+                          <button
+                            onClick={() =>
+                              this.handleTerminarAcuerdo(acuerdo, indice, socket)
+                            }
+                            className={
+                              acuerdo.estadoAcuerdo != 2
+                                ? "w-100 btn btn-danger"
+                                : "w-100 btn btn-danger disabled"
+                            }
+                          >
+                            {trans("acuerdosForm.terminarAcuerdo")}
+                          </button>
+                        </div>
                       </div>
-                      <div className="col-6 text-center">
-                        <span>{trans("notificacionesForm.dias")}</span>
-                        <hr />
-                        <ul className="list-group">
-                          {typeof acuerdo.diasAcordados.map != "undefined"
-                            ? acuerdo.diasAcordados.map(dia => {
-                                return (
-                                  <li className="list-group-item">
-                                    <span className="font-weight-bold">
-                                      {this.traducirDia(dia.dia) + ": "}
-                                    </span>
-                                    <span>
-                                      {dia.horaInicio + " - " + dia.horaFin}
-                                    </span>
-                                  </li>
-                                );
-                              })
-                            : null}
-                        </ul>
-                      </div>
-                    </div>
-                    <div className="row ml-0 mr-0 mt-5">
-                      <button
-                        onClick={() =>
-                          this.handleTerminarAcuerdo(acuerdo, indice)
-                        }
-                        className={
-                          acuerdo.estadoAcuerdo != 2
-                            ? "w-100 btn btn-danger"
-                            : "w-100 btn btn-danger disabled"
-                        }
-                      >
-                        {trans("acuerdosForm.terminarAcuerdo")}
-                      </button>
-                    </div>
+                    </Collapse>
                   </div>
-                </Collapse>
+                );
+              })
+            ) : (
+              <div className="d-flex justify-content-center">
+                <small className="text-danger my-auto">
+                  {trans("acuerdosForm.noData")}
+                </small>
               </div>
-            );
-          })
-        ) : (
-          <div className="d-flex justify-content-center">
-            <small className="text-danger my-auto">
-              {trans("acuerdosForm.noData")}
-            </small>
+            )}
           </div>
         )}
-      </div>
+      </SocketContext.Consumer>
     );
   }
 }
