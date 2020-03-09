d6b8a441033b7de2e5301be36a124bb827d374f7
Jon <jonzubi97@gmail.com>
2020-03-09 22:57:17 +0100

    Solucionando calendario...

--- APP/app_zainduz/src/components/calendarioForm.js
+++ APP/app_zainduz/src/components/calendarioForm.js
@@ -44,8 +44,22 @@ class CalendarioForm extends React.Component {
 
     this.state = {
       jsonEventos: [],
+      displayEventos: [],
       isLoading: true
     };
+    moment.lang('es', {
+      months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+      monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
+      weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
+      weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
+      weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')}
+    );
+  }
+
+  handleOnRangeChange = (dates) => {
+    dates.forEach(date => {
+      console.log(date);
+    });
   }
 
   addDays(date, days) {
@@ -114,6 +128,7 @@ class CalendarioForm extends React.Component {
         defaultView={"week"}
         localizer={localizer}
         events={this.getEventsJson()}
+        onRangeChange={this.handleOnRangeChange}
       />
     );
   }
