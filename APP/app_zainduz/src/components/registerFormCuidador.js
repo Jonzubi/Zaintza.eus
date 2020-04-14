b3ec0acb995393200e93efaba7243e3cc5cab38b
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-04-14 14:05:05 +0200

    Comprobando la existencia del email por la procedure en form cuidador

--- APP/app_zainduz/src/components/registerFormCuidador.js
+++ APP/app_zainduz/src/components/registerFormCuidador.js
@@ -407,14 +407,7 @@ class RegisterForm extends React.Component {
     }
 
     const checkIfEmailExists = await axios.get(
-      `http://${ipMaquina}:3001/api/usuario/`,
-      {
-        params: {
-          filtros: {
-            email: this.state.txtEmail
-          }
-        }
-      }
+      `http://${ipMaquina}:3001/api/procedures/checkIfEmailExists/${this.state.txtEmail}`
     );
 
     if (checkIfEmailExists.data !== "Vacio") {
