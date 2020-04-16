43453cfa03dfdc48d4de7034299dda49776e2142
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-04-16 17:18:23 +0200

    Notificaciones implantado en la cabecera

--- APP/app_zainduz/src/components/header.js
+++ APP/app_zainduz/src/components/header.js
@@ -2,7 +2,11 @@ import React from "react";
 import Headroom from "react-headroom";
 import Avatar from "react-avatar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserCircle, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUserCircle,
+  faGlobe,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
 import { connect } from "react-redux";
 import { toogleMenuPerfil } from "../redux/actions/menuPerfil";
 import { changeFormContent } from "../redux/actions/app";
@@ -10,18 +14,19 @@ import "./styles/header.css";
 import ipMaquina from "../util/ipMaquinaAPI";
 import i18n from "i18next";
 
-const MapDispachToProps = dispatch => {
+const MapDispachToProps = (dispatch) => {
   return {
-    toogleMenuPerfil: payload => dispatch(toogleMenuPerfil(payload)),
-    changeFormContent: form => dispatch(changeFormContent(form))
+    toogleMenuPerfil: (payload) => dispatch(toogleMenuPerfil(payload)),
+    changeFormContent: (form) => dispatch(changeFormContent(form)),
   };
 };
 
-const MapStateToProps = state => {
-  return {    
+const MapStateToProps = (state) => {
+  return {
     direcFoto: state.user.direcFoto,
     nombre: state.user.nombre,
-    apellido1: state.user.apellido1 
+    apellido1: state.user.apellido1,
+    countNotifies: state.notification.countNotifies,
   };
 };
 
@@ -29,43 +34,49 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLogIn: false,
-      hoverLang: ""
+      hoverLang: "",
     };
   }
 
   getAvatar() {
-    return (
-      this.props.direcFoto == "" ?
+    return this.props.direcFoto == "" ? (
       <FontAwesomeIcon
         size="3x"
         style={{ cursor: "pointer", color: "white" }}
         onClick={() => this.props.toogleMenuPerfil(true)}
         icon={faUserCircle}
       />
-      :
-      <Avatar name={this.props.nombre + " " + this.props.apellido1} src={"http://" + ipMaquina + ":3001/api/image/" + this.props.direcFoto}
-              size={50}
-              round={true}
-              onClick={() => this.props.toogleMenuPerfil(true)}
-              style={{ cursor: "pointer", color: "white" }} 
+    ) : (
+      <Avatar
+        name={this.props.nombre + " " + this.props.apellido1}
+        src={"http://" + ipMaquina + ":3001/api/image/" + this.props.direcFoto}
+        size={50}
+        round={true}
+        onClick={() => this.props.toogleMenuPerfil(true)}
+        style={{ cursor: "pointer", color: "white" }}
       />
     );
   }
 
   handleLangHover = (lang) => {
     this.setState({
-      hoverLang: lang
+      hoverLang: lang,
     });
-  }
+  };
 
   handleLangChange = (lang) => {
     i18n.changeLanguage(lang);
-  }
+  };
+
+  handleBellClick = () => {
+    const { changeFormContent } = this.props;
+    changeFormContent("notificaciones");
+  };
 
   render() {
     const IconAvatar = this.getAvatar.bind(this);
     const { hoverLang } = this.state;
+    const { countNotifies, direcFoto } = this.props;
     return (
       <div
         style={{ background: "#343a40" }}
@@ -82,28 +93,50 @@ class Header extends React.Component {
           <h1 className="text-light">Zaintza</h1>
         </a>
         <div className="d-flex flex-row align-items-center">
+          {direcFoto ? (
+            <div
+              onClick={() => this.handleBellClick()}
+              style={{
+                cursor: "pointer",
+              }}
+              className="mr-5"
+            >
+              <FontAwesomeIcon icon={faBell} className="text-white" />
+              {countNotifies > 0 ? (
+                <span className="badge badge-primary rounded-circle ml-1">
+                  {countNotifies}
+                </span>
+              ) : null}
+            </div>
+          ) : null}
+
           <div className="mr-5">
             <FontAwesomeIcon className="text-white mr-2" icon={faGlobe} />
             <span
               style={{
                 cursor: "pointer",
-                textDecoration: hoverLang === "eus" ? "underline" : ""
+                textDecoration: hoverLang === "eus" ? "underline" : "",
               }}
               onMouseEnter={() => this.handleLangHover("eus")}
               onMouseLeave={() => this.handleLangHover("")}
               onClick={() => this.handleLangChange("eus")}
-              className="text-white mr-2">EUS</span>
-            <span
-              className="text-white mr-2">|</span>
+              className="text-white mr-2"
+            >
+              EUS
+            </span>
+            <span className="text-white mr-2">|</span>
             <span
               style={{
                 cursor: "pointer",
-                textDecoration: hoverLang === "es" ? "underline" : ""
+                textDecoration: hoverLang === "es" ? "underline" : "",
               }}
               onMouseEnter={() => this.handleLangHover("es")}
               onMouseLeave={() => this.handleLangHover("")}
               onClick={() => this.handleLangChange("es")}
-              className="text-white">ES</span>
+              className="text-white"
+            >
+              ES
+            </span>
           </div>
           <IconAvatar />
         </div>
