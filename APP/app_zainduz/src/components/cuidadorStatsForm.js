c7fa1d74688e36fc85fd325f360ecca6d4220686
Jon <jonzubi97@gmail.com>
2020-04-30 23:45:14 +0200

    Creado form de cuidadorStats

---
+++ APP/app_zainduz/src/components/cuidadorStatsForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CuidadorFormStats extends React.Component {
+    render(){
+        return(
+            <h5>HEY</h5>
+        )
+    }
+}
+
+export default CuidadorFormStats;
