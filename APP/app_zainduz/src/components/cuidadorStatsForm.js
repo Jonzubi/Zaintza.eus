3aa274d29cf936bc791aea0d2663822ad4b912b2
Jon <jonzubi97@gmail.com>
2020-04-30 23:55:13 +0200

    modelo cuidadorvisita creado

--- APP/app_zainduz/src/components/cuidadorStatsForm.js
+++ APP/app_zainduz/src/components/cuidadorStatsForm.js
@@ -3,7 +3,9 @@ import React from 'react';
 class CuidadorFormStats extends React.Component {
     render(){
         return(
-            <h5>HEY</h5>
+            <div className="p-lg-5 p-2">
+
+            </div>
         )
     }
 }
