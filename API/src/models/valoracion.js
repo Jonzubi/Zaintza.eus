7a7e2e469f7c79ebcf1b590cecff6a1662641dc2
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-05-06 15:01:52 +0200

    idAcuerdo añadido al modelo valoracion

--- API/src/models/valoracion.js
+++ API/src/models/valoracion.js
@@ -1,26 +1,33 @@
-module.exports = conexion => {
-    const Schema = conexion.Schema;
-  
-    var valoracion = new Schema({
-      idUsuario: {
-          type: String,
-          required: true,
-          ref: 'Usuario'
-      },
-      //idValorador es el idUsuario del valorador
-      idValorador: {
-        type: String,
-        required: true,
-        ref: 'Usuario'
-      },
-      valor: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 5
-      },
-      comentario: String
-    });
-  
-    return conexion.model("Valoracion", valoracion, "Valoraciones");
-  };
+module.exports = (conexion) => {
+  const Schema = conexion.Schema;
+
+  var valoracion = new Schema({
+    idUsuario: {
+      type: String,
+      required: true,
+      ref: "Usuario",
+    },
+    //idValorador es el idUsuario del valorador
+    idValorador: {
+      type: String,
+      required: true,
+      ref: "Usuario",
+    },
+    //idAcuerdo es el acuerdo del que proviene la valoracion
+    // ya que las valoraciones se podran hacer despues de terminar un acuerdo
+    idAcuerdo: {
+      type: String,
+      required: true,
+      ref: "Usuario",
+    },
+    valor: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 5,
+    },
+    comentario: String,
+  });
+
+  return conexion.model("Valoracion", valoracion, "Valoraciones");
+};
