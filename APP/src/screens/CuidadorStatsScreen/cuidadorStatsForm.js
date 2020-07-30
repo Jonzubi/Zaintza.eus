4f8ad050f521c8e1ac12ca50c7b0ca2096709efa
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-07-30 14:32:16 +0200

    Estadisticas funcionando, toca estilizarlo

--- APP/src/screens/CuidadorStatsScreen/cuidadorStatsForm.js
+++ APP/src/screens/CuidadorStatsScreen/cuidadorStatsForm.js
@@ -7,15 +7,19 @@ import cogoToast from "cogo-toast";
 import { trans } from "../../util/funciones";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
+import i18next from "i18next";
 
 class CuidadorFormStats extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      visitasConLogin: 0,
-      visitasSinLogin: 0,
+      visitasConLogin: [],
+      visitasSinLogin: [],
     };
+    this.chartReference = React.createRef();
   }
   componentDidMount() {
     const { email, contrasena, idPerfil } = this.props;
@@ -40,8 +44,103 @@ class CuidadorFormStats extends React.Component {
         });
       });
   }
+
+  getChartDataVisitas = (nameArray) => {
+    // name array será "visitasConLogin" o "visitasSinLogin"
+    let savedMonths = [];
+
+    this.state[nameArray].map(visita => {
+      const fechaVisto  = moment(visita.fechaVisto);
+      const isMonthSaved = savedMonths.find(saved => saved.month === fechaVisto.month());
+      if (!isMonthSaved) {
+        savedMonths.push({
+          month: fechaVisto.month(),
+          countVisitas: 1
+        })
+      } else {
+        savedMonths = savedMonths.map(saved => {
+          if (saved.month === fechaVisto.month()) {
+            saved.countVisitas += 1;
+          }
+
+          return saved;
+        })
+      }
+    });
+    
+    return savedMonths;
+  }
+
+  getMonthLabels = () => {
+    const visitasSinLogin = this.getChartDataVisitas('visitasSinLogin');
+    const visitasConLogin = this.getChartDataVisitas('visitasConLogin');
+    const monthLabels = [];
+
+    // Con el primer array no habrá repetidos
+    visitasSinLogin.map(visita => {
+      monthLabels.push(i18next.t(`meses.${visita.month}`));
+    });
+
+    visitasConLogin.map(visita => {
+      const label = i18next.t(`meses.${visita.month}`);
+
+      if (!monthLabels.includes(label)) {
+        monthLabels.push(label);
+      }
+    })
+
+    return monthLabels;
+  }
+
+  getChartData = (canvas) => {
+    return {
+      labels: this.getMonthLabels(),
+      datasets: [{
+        label: i18next.t('cuidadorStatsForm.visitasSinLogin'),
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: this.getChartDataVisitas('visitasSinLogin').map(visita => visita.countVisitas)
+      },{
+        label: i18next.t('cuidadorStatsForm.visitasConLogin'),
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: this.getChartDataVisitas('visitasConLogin').map(visita => visita.countVisitas)
+      }]
+    }
+  }
   render() {
     const { isLoading, visitasConLogin, visitasSinLogin } = this.state;
+    const data = this.getChartData;
     return (
       <div
         style={
@@ -107,6 +206,14 @@ class CuidadorFormStats extends React.Component {
                 <FontAwesomeIcon icon={faEye} className="ml-1" />
               </div>
             </div>
+            <div
+              style={{
+                width: window.innerWidth < 991 ? window.innerWidth : window.innerWidth * 0.6,
+                height: 400
+              }}
+            >
+              <Line data={data} />
+            </div>
           </div>
         )}
       </div>
@@ -118,6 +225,7 @@ const mapStateToProps = (state) => ({
   email: state.user.email,
   contrasena: state.user.contrasena,
   idPerfil: state.user._id,
+  nowLang: state.app.nowLang
 });
 
 export default connect(mapStateToProps)(CuidadorFormStats);
