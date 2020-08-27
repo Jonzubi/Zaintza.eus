7b9ce7983ab823f77bdd237b8804f5bab9043f2d
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 10:29:12 +0200

    Otra posible solucion en serveApp

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -1,27 +1,19 @@
 const express = require("express");
-const path = require('path');
-const fs = require("fs");
+const path = require("path");
 const app = express();
 const port = 80;
 
-fs.readFile("./build/index.html", (err, html) => {
-  if (err) {
-    throw err;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (req, res) => {
+  if (req.get("X-Forwarded-Proto") == "https" || req.hostname == "localhost") {
+    console.log("rspondifen");
+    res.sendFile(path.join(__dirname + "/build/index.html"));
+  } else if (
+    req.get("X-Forwarded-Proto") != "https" &&
+    req.get("X-Forwarded-Port") != "443"
+  ) {
+    res.redirect("https://www.zaintza.eus");
   }
-
-  app.get("/", (req, res) => {
-    if (
-      req.get("X-Forwarded-Proto") == "https" ||
-      req.hostname == "localhost"
-    ) {
-      res.sendFile(path.join(__dirname + '/build/index.html'));
-    } else if (
-      req.get("X-Forwarded-Proto") != "https" &&
-      req.get("X-Forwarded-Port") != "443"
-    ) {
-      res.redirect("https://zaintza.eus");
-    }
-  });
-
-  app.listen(port, () => console.log(`Zaintza => ${port}`));
 });
+
+app.listen(port, () => console.log(`Zaintza => ${port}`));
