e9ab16eb7ff540df2454f8d90aadf5c5f62e79ee
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 08:45:44 +0200

    Si hace un HTTP redirige a HTTPS

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -5,7 +5,11 @@ const port = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
-  console.log(req);
+  const port = req.socket.localPort;
+  if (port === 80) {
+    res.redirect('https://www.zaintza.eus');
+    return;
+  }
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
