5be59a0473e234e929cea45bb9be0da684658bd8
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 09:30:28 +0200

    El serveApp crea dos instancias, uno de http y otro de https

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -1,22 +1,23 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
-const port = 80;
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-fs.readFile('./build/index.html', (err, html) => {
+fs.readFile("./build/index.html", (err, html) => {
   if (err) {
     throw err;
   }
-  app.get('/', function(req, res) {
-    const secure = req.secure;
-    if (!secure) {
-      res.redirect('https://www.zaintza.eus');
-      return;
-    }
-    res.writeHeader(200, {"Content-Type": "text/html"});  
-    res.write(html);  
-    res.end();  
-  });
 
-  app.listen(port, () => console.log(`Zaintza => ${port}`));
+  https.createServer((req, res) => {
+    res.writeHeader(200, { "Content-Type": "text/html" });
+    res.write(html);
+    res.end();
+  }).listen(443);
+
+  http
+    .createServer(function (req, res) {
+      res.writeHead(301, { Location: "https://www.zaintza.eus" });
+      res.end();
+    })
+    .listen(80);
 });
