31d7b3f7cf537d1353265d15487f8662f79951d2
Jon <jonzubi97@gmail.com>
2020-08-27 22:11:44 +0200

    serveApp ya solo escucha el 443

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = 443;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("/", (req, res) => {
-  if (req.get("X-Forwarded-Proto") == "https" || req.hostname == "localhost") {
-    console.log("rspondifen");
-    res.sendFile(path.join(__dirname + "/build/index.html"));
-  } else if (
-    req.get("X-Forwarded-Proto") != "https" &&
-    req.get("X-Forwarded-Port") != "443"
-  ) {
-    res.redirect("https://www.zaintza.eus");
-  }
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Zaintza => ${port}`));
