b46abb266c1023c49260ccb523273d3c75c475c6
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 09:08:24 +0200

    Posible solucion a la redireccion hecho

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 80;
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-  const port = req.socket.localPort;
-  if (port === 80) {
-    res.redirect('https://www.zaintza.eus');
-    return;
+fs.readFile('./build/index.html', (err, html) => {
+  if (err) {
+    throw err;
   }
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  app.get('/', function(req, res) {
+    const port = req.socket.localPort;
+    if (port === 80) {
+      res.redirect('https://www.zaintza.eus');
+      return;
+    }
+    res.writeHeader(200, {"Content-Type": "text/html"});  
+    res.write(html);  
+    res.end();  
+  });
 
-app.listen(port, () => console.log(`Zaintza => ${port}`));
+  app.listen(port, () => console.log(`Zaintza => ${port}`));
+});
