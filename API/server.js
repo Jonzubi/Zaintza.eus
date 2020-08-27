4d3d18991ed471bd62c10cac20c1a634c77ac536
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 10:45:42 +0200

    API con claves SSL

--- API/server.js
+++ API/server.js
@@ -3,7 +3,9 @@ const express = require("express"),
   cors = require("cors"),
   bodyParser = require("body-parser"),
   port = 3001,
-  conexion = require("./util/bdConnection");
+  conexion = require("./util/bdConnection"),
+  fs = require('fs'),
+  https = require('https');
 
 app.use(cors());
 app.use(bodyParser.json({limit: "50mb"}));
@@ -13,6 +15,8 @@ var routes = require("./src/routes/ruteo");
 let modelos = require("./util/requireAllModels")(conexion)
 routes(app, modelos);
 
-app.listen(port, function() {
-  console.log("[API] Escuchando en el puerto : " + port);
-});
+https.createServer({
+  key: fs.readFileSync('./SSL/kep.pem'),
+  cert: fs.readFileSync('./SSL/cert.pem'),
+  passphrase: 'jonzaintza'
+}, app).listen(port, () => console.log(`[API] Escuchando en el puerto : ${port}`));
