5c9a67a3baa3dad58324624ce4ff167bff50c915
Jon <jonzubi97@gmail.com>
2020-08-27 20:36:38 +0200

    Probando axios con la configuracion del SSL

--- APP/src/util/axiosInstance.js
+++ APP/src/util/axiosInstance.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import https from 'https';
+import fs from 'fs';
 
-const instance = axios.create();
+const instance = axios.create({
+  key: fs.readFileSync('../SSL/key.pem'),
+  cert: fs.readFileSync('../SSL/cert.pem'),
+  passphrase: 'jonzaintza'
+});
 
 instance.defaults.httpsAgent = new https.Agent({ rejectUnauthorized:false });
 
