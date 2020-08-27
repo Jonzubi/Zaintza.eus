fbc735cb1666bc850ec51377782190ddc71c7fdb
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-27 08:34:25 +0200

    Log en serveApp

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -5,6 +5,7 @@ const port = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
+  console.log(req);
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
