d77cb76018fe1b19b6095160fb5ea18fb4fe5296
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-28 13:47:20 +0200

    La redireccion de HTTP a HTTPS funciona

--- APP/serveApp.js
+++ APP/serveApp.js
@@ -2,8 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
-const port = 80;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get("/", (req, res) => {
@@ -13,5 +13,12 @@ app.get("/", (req, res) => {
 https.createServer({
   key: fs.readFileSync('./src/SSL/key.pem'),
   cert: fs.readFileSync('./src/SSL/cert.pem')
-}, app).listen(443, () => console.log("listening"));
-//app.listen(port, () => console.log(`Zaintza => ${port}`));
+}, app).listen(443);
+
+http.createServer((req, res) => {
+  res.writeHead(301,
+    {Location: 'https://www.zaintza.eus'}
+  );
+  res.end();
+  return;
+}).listen(80);
