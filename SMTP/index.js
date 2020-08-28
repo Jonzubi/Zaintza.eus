8c21e6a4af752833caa32e0ab1db2865be24424f
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-08-28 12:40:26 +0200

    Socket y SMTP con HTTPS

--- SMTP/index.js
+++ SMTP/index.js
@@ -1,16 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3003;
+const https = require("https");
+const fs = require("fs");
 
 app.use(cors());
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({extended: true, limit:"50mb"}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 const routes = require("./routes/ruteo");
 routes(app);
 
-app.listen(port, () => {
-    console.log(`[SMTP] Escuchando el puerto : ${port}`);
-})
+const https = require("https").createServer(
+  {
+    key: fs.readFileSync("./SSL/key.pem"),
+    cert: fs.readFileSync("./SSL/cert.pem"),
+  },
+  app
+);
+
+https.listen(port, () => {
+  console.log(`[SMTP] Escuchando el puerto : ${port}`);
+});
