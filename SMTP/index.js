07ea43078a0e2af9d137ecf20ca8a9b842b8f16e
Jon Zubizarreta <jonzubi97@gmail.com>
2020-09-17 22:52:52 +0200

    Error con const eliminado

--- SMTP/index.js
+++ SMTP/index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = 3003;
-const https = require("https");
 const fs = require("fs");
 
 app.use(cors());
