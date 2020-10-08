a9be21455663706b306026843a7566ce8dbe5f1a
Jon Zubizarreta <jonzubi97@gmail.com>
2020-10-08 22:10:14 +0200

    Condicionando movil para aplicar absolute

--- APP/src/components/footer.js
+++ APP/src/components/footer.js
@@ -3,21 +3,28 @@ import { trans } from "../util/funciones";
 import { colors } from "../util/colors";
 import { useDispatch } from "react-redux";
 import { changeFormContent } from "../redux/actions/app";
+import { isMobile } from 'react-device-detect';
 
 const Footer = () => {
   const dispatch = useDispatch();
   const handleAvisoLegalClick = () => {
     dispatch(changeFormContent("avisoLegal"));
   };
+
+  let footerStyle = {
+    background: colors.grey,
+    fontSize: 11
+  };
+  if (isMobile) {
+    footerStyle = Object.assign({}, footerStyle, {
+      position: 'relative',
+      bottom: 0,
+      width: '100%'
+    })
+  }
   return (
     <div
-      style={{
-        background: colors.grey,
-        fontSize: 11,
-        position: 'absolute',
-        bottom: 0,
-        width: '100%'
-      }}
+      style={footerStyle}
       className="d-flex flex-row align-itesm-center justify-content-center p-1 text-white"
     >
       <span
