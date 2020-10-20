c859ee1bebaed5d2b1ecd2476e7cfbdb70c526e0
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-10-20 15:13:25 +0200

    Footer sombra puesta, changeLang color cambiado

--- APP/src/components/footer.js
+++ APP/src/components/footer.js
@@ -18,9 +18,10 @@ const Footer = () => {
     <div
       style={{
         background: colors.grey,
-        fontSize: 11
+        fontSize: 11,
+        boxShadow: '0 -.125rem .25rem rgba(0,0,0,.075)'
       }}
-      className="d-flex flex-row align-items-center justify-content-center p-1 text-white"
+      className="d-flex flex-row align-items-center justify-content-center p-2 bg-white"
     >
       <span
         onClick={handleAvisoLegalClick}
