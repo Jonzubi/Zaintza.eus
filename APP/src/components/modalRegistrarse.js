7474f7967162a5bb3d03e2b80f2c562549f0ef56
Jon Zubizarreta <jonzubi97@gmail.com>
2020-10-26 21:06:00 +0100

    ModalRegistrarse cambiado a Hooks

--- APP/src/components/modalRegistrarse.js
+++ APP/src/components/modalRegistrarse.js
@@ -5,43 +5,31 @@ import ModalTitle from "react-bootstrap/ModalTitle";
 import ModalBody from "react-bootstrap/ModalBody";
 import ModalFooter from "react-bootstrap/ModalFooter";
 import Button from "react-bootstrap/Button";
-import { connect } from "react-redux";
-import {toogleModal} from "../redux/actions/modalRegistrarse";
-import {changeFormContent} from "../redux/actions/app";
+import { Slide } from "@material-ui/core";
+import { connect, useDispatch, useSelector } from "react-redux";
+import { toogleModal } from "../redux/actions/modalRegistrarse";
+import { changeFormContent } from "../redux/actions/app";
 import "./styles/modalRegistrarse.css";
 
-const mapStateToProps = state => {
-  return {
-    showModal: state.modalRegistrarse.showModal
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        toogleModal: (payload) => dispatch(toogleModal(payload)),
-        changeFormContent: (payload) => dispatch(changeFormContent(payload))
-    };
+const ModalRegistrarse = () => {
+  const dispatch = useDispatch();
+  const showModal = useSelector(state => state.modalRegistrarse.showModal);
+  return (
+    <Modal className="modalRegistrarse" show={showModal} onHide={() => dispatch(toogleModal(false))}>
+      <ModalHeader closeButton>
+        <ModalTitle>Izen ematea</ModalTitle>
+      </ModalHeader>
+      <ModalBody className="d-flex flex-row align-items-center justify-content-center">Nola nahiko zenuke izena eman, guraso gisa ala zaintzaile gisa?</ModalBody>
+      <ModalFooter className="d-flex flex-row align-items-center justify-content-between">
+        <Button className="" variant="success" onClick={() => { dispatch(toogleModal(false)); dispatch(changeFormContent("registrarCuidador")); }}>
+          Zaintzaile gisa
+          </Button>
+        <Button className="" variant="success" onClick={() => { dispatch(toogleModal(false)); dispatch(changeFormContent("registrarCliente")); }}>
+          Guraso gisa
+          </Button>
+      </ModalFooter>
+    </Modal>
+  );
 }
 
-class ModalRegistrarse extends React.Component {
-  render() {
-    return (
-      <Modal className="modalRegistrarse" show={this.props.showModal} onHide={() => this.props.toogleModal(false)}>
-        <ModalHeader closeButton>
-          <ModalTitle>Izen ematea</ModalTitle>
-        </ModalHeader>
-        <ModalBody className="d-flex flex-row align-items-center justify-content-center">Nola nahiko zenuke izena eman, guraso gisa ala zaintzaile gisa?</ModalBody>
-        <ModalFooter className="d-flex flex-row align-items-center justify-content-between">
-          <Button className="" variant="success" onClick={() => {this.props.toogleModal(false);this.props.changeFormContent("registrarCuidador")}}>
-            Zaintzaile gisa
-          </Button>
-          <Button className="" variant="success" onClick={() => {this.props.toogleModal(false);this.props.changeFormContent("registrarCliente");}}>
-            Guraso gisa
-          </Button>
-        </ModalFooter>
-      </Modal>
-    );
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ModalRegistrarse);
+export default ModalRegistrarse;
