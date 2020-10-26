6f21c45002f17e4b5b6c15af12fc8642559f2981
Jon Zubizarreta <jonzubi97@gmail.com>
2020-10-26 21:17:53 +0100

    ModalRegistrarse arreglado

--- APP/src/components/modalRegistrarse.js
+++ APP/src/components/modalRegistrarse.js
@@ -1,34 +1,38 @@
 import React from "react";
-import Modal from "react-bootstrap/Modal";
-import ModalHeader from "react-bootstrap/ModalHeader";
-import ModalTitle from "react-bootstrap/ModalTitle";
-import ModalBody from "react-bootstrap/ModalBody";
-import ModalFooter from "react-bootstrap/ModalFooter";
-import Button from "react-bootstrap/Button";
-import { Slide } from "@material-ui/core";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { Slide, Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
 import { toogleModal } from "../redux/actions/modalRegistrarse";
 import { changeFormContent } from "../redux/actions/app";
+import { trans } from '../util/funciones';
 import "./styles/modalRegistrarse.css";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const ModalRegistrarse = () => {
   const dispatch = useDispatch();
   const showModal = useSelector(state => state.modalRegistrarse.showModal);
   return (
-    <Modal className="modalRegistrarse" show={showModal} onHide={() => dispatch(toogleModal(false))}>
-      <ModalHeader closeButton>
-        <ModalTitle>Izen ematea</ModalTitle>
-      </ModalHeader>
-      <ModalBody className="d-flex flex-row align-items-center justify-content-center">Nola nahiko zenuke izena eman, guraso gisa ala zaintzaile gisa?</ModalBody>
-      <ModalFooter className="d-flex flex-row align-items-center justify-content-between">
+    <Dialog
+      open={showModal}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={() => dispatch(toogleModal(false))}
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{trans('modalRegistrarse.titulo')}</DialogTitle>
+      <DialogContent>{trans('modalRegistrarse.content')}</DialogContent>
+      <DialogActions>
         <Button className="" variant="success" onClick={() => { dispatch(toogleModal(false)); dispatch(changeFormContent("registrarCuidador")); }}>
-          Zaintzaile gisa
-          </Button>
+          {trans('modalRegistrarse.comoCuidador')}
+        </Button>
         <Button className="" variant="success" onClick={() => { dispatch(toogleModal(false)); dispatch(changeFormContent("registrarCliente")); }}>
-          Guraso gisa
-          </Button>
-      </ModalFooter>
-    </Modal>
+          {trans('modalRegistrarse.comoCliente')}
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
 
