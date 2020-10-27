dd3e85c5159d7bb0f84f7a1d5b70d924855131bc
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-10-27 15:10:24 +0100

    El server se puede ejecutar tanto en modo HTTPS como en HTTP

--- API/server.js
+++ API/server.js
@@ -4,18 +4,34 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   port = 3001,
   conexion = require("./util/bdConnection"),
-  fs = require('fs'),
-  https = require('https');
+  fs = require("fs"),
+  https = require("https"),
+  http = require("http");
 
 app.use(cors());
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({extended: true, limit:"50mb"}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 var routes = require("./src/routes/ruteo");
-let modelos = require("./util/requireAllModels")(conexion)
+let modelos = require("./util/requireAllModels")(conexion);
 routes(app, modelos);
-
-https.createServer({
-  key: fs.readFileSync('./SSL/key.pem'),
-  cert: fs.readFileSync('./SSL/cert.pem'),
-}, app).listen(port, () => console.log(`[API] Escuchando en el puerto : ${port}`));
+// El includes lo hago porque me está devolviendo production con un espacio en blanco al final
+if (process.env.NODE_ENV.includes("production")) {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync("./SSL/key.pem"),
+        cert: fs.readFileSync("./SSL/cert.pem"),
+      },
+      app
+    )
+    .listen(port, () =>
+      console.log(`[API - HTTPS] Escuchando en el puerto : ${port}`)
+    );
+} else {
+  http
+    .createServer(app)
+    .listen(port, () =>
+      console.log(`[API - HTTP] Escuchando en el puerto : ${port}`)
+    );
+}
