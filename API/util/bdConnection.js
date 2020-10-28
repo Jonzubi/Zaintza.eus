02594617ae4bb52cdb115a6c42333b11a222f748
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-10-28 15:21:45 +0100

    En modo develop se conecta a la base de datos DEV

--- API/util/bdConnection.js
+++ API/util/bdConnection.js
@@ -2,10 +2,12 @@ let mongoose = require("mongoose");
 const pwdAtlas = require("./mongoDBAtlasPWD");
 const ipMaquina = require("./ipMaquina");
 //Esta es para el Atlas
+//MODO PRODUCTION
+let uri = `mongodb+srv://admin:${pwdAtlas}@cluster0-sfybj.mongodb.net/zaintza?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://admin:${pwdAtlas}@cluster0-sfybj.mongodb.net/zaintza?retryWrites=true&w=majority`;
-//Esta es la local
-//const uri = 'mongodb://' + ipMaquina + ':27017/zaintza';
+// MODO DEV
+uri = `mongodb+srv://admin:${pwdAtlas}@cluster0.6boix.mongodb.net/zaintza?retryWrites=true&w=majority`
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
