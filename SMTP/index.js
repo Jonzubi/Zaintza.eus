b1cddb4ca445a2f9acc52a023fdcb2b37960395b
Jon zubizarreta <j.zubizarreta@e-coordina.com>
2020-10-30 11:09:41 +0100

    Entorno desarrollo para SOCKET y SMTP creado

--- SMTP/index.js
+++ SMTP/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const https = require("https");
+const http = require("http");
 const bodyParser = require("body-parser");
 const port = 3003;
 const fs = require("fs");
@@ -12,14 +14,20 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 const routes = require("./routes/ruteo");
 routes(app);
 
-const https = require("https").createServer(
-  {
-    key: fs.readFileSync("./SSL/key.pem"),
-    cert: fs.readFileSync("./SSL/cert.pem"),
-  },
-  app
-);
-
-https.listen(port, () => {
-  console.log(`[SMTP] Escuchando el puerto : ${port}`);
-});
+if (process.env.NODE_ENV.includes("production")) {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync("./SSL/key.pem"),
+        cert: fs.readFileSync("./SSL/cert.pem"),
+      },
+      app
+    )
+    .listen(port, () => {
+      console.log(`[SMTP - HTTPS] Escuchando el puerto : ${port}`);
+    });
+} else {
+  http.createServer(app).listen(port, () => {
+    console.log(`[SMTP - HTTP] Escuchando el puerto : ${port}`);
+  });
+}
