7a794de36fb5b3e73c80f1374053075c5e18b051
Jon Zubizarreta <jonzubi97@gmail.com>
2021-02-04 21:43:33 +0100

    Socket cliente actualizado

--- APP/src/App.js
+++ APP/src/App.js
@@ -44,6 +44,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.socket = io(`${protocol}://${ipMaquina}:3002`, {
+      withCredentials: true,
       query: `deviceData=${JSON.stringify({
         isMobile,
         osName
