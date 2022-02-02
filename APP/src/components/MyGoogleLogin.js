924b05de0921a75732e3058187d944ef68ee2873
Jon Zubizarreta <jonzubi97@gmail.com>
2022-02-03 00:10:23 +0100

    Intento logear el resultado, no va por alguna razon

--- APP/src/components/MyGoogleLogin.js
+++ APP/src/components/MyGoogleLogin.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
 
-const MyGoogleLogin = ({ onSuccess, onFailure }) => {
+const MyGoogleLogin = () => {
     const onSuccessGoogle = (response) => {
-        //onSuccess(response);
+        console.log(response);
     };
 
     const onFailureGoogle = (response) => {
-        //onFailure(response);
+        console.log(response);
     };
     
     return (
         <GoogleLogin
         clientId="1028885794620-71epbcb0u0k7fe6ia2h2ojmb0ebm8rd8.apps.googleusercontent.com"
         buttonText="Login / Registrarse"
-        onSuccess={onSuccess}
-        onFailure={onFailure}
+        onSuccess={onSuccessGoogle}
+        onFailure={onFailureGoogle}
         cookiePolicy={"single_host_origin"}
         />
     );
