b609054e21a71c5455b719387296f8409322706b
Jon Zubizarreta <jonzubi97@gmail.com>
2022-02-04 18:51:15 +0100

    Creo traduccion para el boton de Google

--- APP/src/components/MyGoogleLogin.js
+++ APP/src/components/MyGoogleLogin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
+import { trans } from "../util/funciones";
 
 const MyGoogleLogin = () => {
     const onSuccessGoogle = (response) => {
@@ -12,11 +13,11 @@ const MyGoogleLogin = () => {
     
     return (
         <GoogleLogin
-        clientId="1028885794620-71epbcb0u0k7fe6ia2h2ojmb0ebm8rd8.apps.googleusercontent.com"
-        buttonText="Login / Registrarse"
-        onSuccess={onSuccessGoogle}
-        onFailure={onFailureGoogle}
-        cookiePolicy={"single_host_origin"}
+            clientId="1028885794620-71epbcb0u0k7fe6ia2h2ojmb0ebm8rd8.apps.googleusercontent.com"
+            buttonText={trans('loginForm.googleLogin')}
+            onSuccess={onSuccessGoogle}
+            onFailure={onFailureGoogle}
+            cookiePolicy={"single_host_origin"}
         />
     );
 }
